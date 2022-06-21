ef959726224d5d3e75da64fdb782a1cfb3eb5ac1
buildanetwork <jackferns0312@hotmail.com>
2022-06-21 20:02:39 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
